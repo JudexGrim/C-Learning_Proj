1165213d0ea4a5261afe1b0b6c5dba499a79282f
JudexGrim <om.jumaili01@gmail.com>
2024-08-14 23:54:24 +0300

    Added Excel Reader Project To Main

---
+++ Excel-Reader/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Reader
+{
+    internal class Entry
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Salary { get; set; }
+
+        public void DisplayEntry() 
+        {
+            Console.WriteLine(ID + "\t" + Name+ "\t\t\t" + Role + "\t\t\t\t" + Salary);
+        }
+    
+    }
+    
+}
---
+++ Excel-Reader/Program.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.ExtendedProperties;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+using Excel_Reader;
+using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Set File path
+            string filePath = "";
+            List<Entry> entries = new List<Entry>();
+
+            //Selecting A File
+            while (filePath == "")
+            {
+                using (OpenFileDialog Ofd = new OpenFileDialog { Filter = "Excel Files (*.xlsx;*.xls)|*.xlsx;*.xls|All Files (*.*)|*.*", Title = "Select A File" })
+                {
+                    if (Ofd.ShowDialog() == DialogResult.OK) filePath = Ofd.FileName;
+                }
+            }
+
+            //Getting Info From Excel File
+            using (var workbook = new XLWorkbook(filePath)) 
+            {
+                var worksheet = workbook.Worksheet(1); 
+                var range = worksheet.RangeUsed(); 
+
+                foreach (var row in range.RowsUsed().Skip(1))
+                {
+                    //Adds a New Entry Object To The List
+                    entries.Add(new Entry());
+                    //Initializes Props
+                    entries.Last().ID = row.Cell(1).GetValue<int>();
+                    entries.Last().Name = row.Cell(2).GetValue<string>();
+                    entries.Last().Role = row.Cell(3).GetValue<string>();
+                    entries.Last().Salary = row.Cell(4).GetValue<string>();
+                }
+
+                //Legend
+                foreach (var cell in range.Row(1).Cells())
+                {
+                    Console.Write(cell.Value + "\t\t\t");
+                }
+                Console.WriteLine("");
+
+                //Diplay Entry List
+                foreach (Entry entry in entries)
+                {
+                    entry.DisplayEntry();
+                }
+                
+            }
+
+        }
+    }
+}
